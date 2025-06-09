e2ef6817109684790dfeadd370cfa2de7b1331db
Moritz Körber <koerber.moritz@gmail.com>
2025-06-09 14:45:50 +0200

    Add data quality check

--- dagster_home/local_dagster/assets.py
+++ dagster_home/local_dagster/assets.py
@@ -12,7 +12,7 @@ from dagster_duckdb import DuckDBResource
 from .project import local_dagster
 
 
-@asset(compute_kind="python")
+@asset(compute_kind="python", group_name="ingest")
 def area1(context: AssetExecutionContext, duckdb: DuckDBResource) -> None:
     with duckdb.get_connection() as conn:
         table_name = "area1"
@@ -27,6 +27,22 @@ def area1(context: AssetExecutionContext, duckdb: DuckDBResource) -> None:
         result = conn.execute(f"select count(*) from {schema}.{table_name}").fetchone()
         # Log some metadata about the table we just wrote
         context.add_output_metadata({"num_rows": result[0]})
+
+
+@asset_check(asset=area1)
+def area1_label_check(duckdb: DuckDBResource) -> AssetCheckResult:
+    with duckdb.get_connection() as conn:
+        query_result = conn.execute(
+            """
+            select count(*) from local.area1
+            where label != 0
+            """
+        ).fetchone()
+
+        count = query_result[0] if query_result else 0
+        return AssetCheckResult(
+            passed=count == 0, metadata={"missing dimensions": count}
+        )
 
 
 # A Dagster asset that, when materialized, will execute dbt build for the specified dbt project, effectively running all your dbt models and tests.
--- dagster_home/local_dagster/definitions.py
+++ dagster_home/local_dagster/definitions.py
@@ -4,12 +4,13 @@ from dagster import Definitions
 from dagster_dbt import DbtCliResource
 from dagster_duckdb import DuckDBResource
 
-from .assets import area1, local_dagster_assets
+from .assets import area1, area1_label_check, local_dagster_assets
 from .project import local_dagster
 from .schedules import schedules
 
 defs = Definitions(
     assets=[local_dagster_assets, area1],
+    asset_checks=[area1_label_check],
     schedules=schedules,
     # In Dagster, Resources are the external services, tools, and storage backends
     # you need to do your job. For the storage backend in this project, we'll use DuckDB
