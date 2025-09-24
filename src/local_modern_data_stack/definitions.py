0e8037fce813f66fd85b9882bd26329dac57b6b5
Moritz Körber <koerber.moritz@gmail.com>
2025-09-24 20:55:01 +0200

    Restructuring

--- src/local_modern_data_stack/definitions.py
+++ src/local_modern_data_stack/definitions.py
@@ -1,16 +1,17 @@
 """
 This file centralizes the definitions of your Dagster assets, schedules, and resources,
-making them available to Dagster tools like the UI and CLI.
+making them available to Dagster tools like the UI and CLI. It is the entry point that
+Dagster will load when deploying your code location.
 """
 
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 from dagster_duckdb import DuckDBResource
 
-from .assets.bronze import covid19_data_rki
-from .assets.dbt import dbt_assets
-from .assets.presentation import cases_barchart
-from .project import local_dagster
+from .defs.assets.bronze import covid19_data_rki
+from .defs.assets.dbt import dbt_assets
+from .defs.assets.presentation import cases_barchart
+from .defs.project import local_dagster
 from .schedules import schedules
 
 defs = Definitions(
--- src/local_modern_data_stack/defs/project.py
+++ src/local_modern_data_stack/defs/project.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 local_dagster = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..", "..", "..", "dbt").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
 )
 local_dagster.prepare_if_dev()
